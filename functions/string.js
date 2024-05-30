dcc80c12ddbca5444dbd3668eb1a6947c34ee7aa
M Vishok <vishokmanikantan@gmail.com>
2024-05-30 12:33:03 +0530

    refactor: Update replace function to support global replacement
    ```

--- functions/string.js
+++ functions/string.js
@@ -23,12 +23,16 @@ function trim(string) {
     return string.trim()
 }
 
-function replace(string, args) {
+function replaceOne(string, args) {
     return string.replace(args[0], args[1])
+}
+
+function replace(string, args) {
+    return string.replace(new RegExp(args[0], 'g'), args[1])
 }
 
 function split(string, args) {
     return string.split(args[0])
 }
 
-export { lower, upper, capitalize, reverse, length, trim, replace, split }
+export { lower, upper, capitalize, reverse, length, trim, replaceOne, replace, split }
