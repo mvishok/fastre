f6f3641bede1647adfaa6ed79553dc61c6944c13
mvishok <vishokmanikantan@gmail.com>
2024-06-10 14:36:23 +0530

    refactor: Improve strRender function for better handling

--- template/string.js
+++ template/string.js
@@ -1,57 +1,58 @@
 import fn from "../functions/caller.js";
 import { data } from "../storage/unique.js"
+import { log } from "../modules/log.js";
 
 export function strRender(str){
-    str = str.split(" ")
     
-    for (let i = 0; i < str.length; i++) {
-        //if it is a variable present in the data object
-        if (data[str[i]]){
-            str[i] = data[str[i]]
-            continue;
-        } 
-        //if it is a array access (allow multiple levels)
-        if (str[i].includes("[") && str[i].endsWith("]")){
-            let keys = str[i].split("[")
-            let v = data[keys[0]]
-            for (let j = 1; j < keys.length; j++) {
-                v = v[keys[j].replace("]", "")];
-                if (v===undefined) break;
-            }
-            str[i] = v;
-            continue;
+    //if it is a variable present in the data object
+    if (data[str]){
+        str = data[str]
+        return str;
+    } 
+    //if it is a array access (allow multiple levels)
+    if (str.includes("[") && str.endsWith("]")){
+        let keys = str.split("[")
+        let v = data[keys[0]]
+        for (let j = 1; j < keys.length; j++) {
+            v = v[keys[j].replace("]", "")];
+            if (v===undefined) break;
         }
-        //if it is a literal representation
-        if (str[i].startsWith('"') && str[i].endsWith('"') || str[i].startsWith("'") && str[i].endsWith("'")){
-            continue;
-        }
-        //if it is a number
-        if (!isNaN(str[i])){
-            continue;
-        }
-        //if it is a boolean, true or false replace with 0 or 1
-        if (str[i] == "true"){
-            str[i] = 1;
-            continue;
-        } else if (str[i] == "false"){
-            str[i] = 0;
-            continue;
-        }
-
-        //if it is a function call
-        if (str[i].includes("(") && str[i].endsWith(")")){
-            //args = between first open parenthesis and last close parenthesis, split by comma
-            let args = str[i].slice(str[i].indexOf("(")+1, str[i].lastIndexOf(")"))
-            args = args.split(",")
-            args = args.map(arg => arg.trim()) //remove whitespace
-            for (let j = 0; j < args.length; j++) {
-                args[j] = strRender(args[j]);
-            }
-
-            const func = str[i].slice(0, str[i].indexOf("("))
-            str[i] = fn(func, args)
-            continue;
-        }
+        str = v;
+        return str;
     }
-    return str.join(" ")
+    //if it is a literal representation
+    if (str.startsWith('"') && str.endsWith('"') || str.startsWith("'") && str.endsWith("'")){
+        return str;
+    }
+    //if it is a number
+    if (!isNaN(str)){
+        return Number(str);
+    }
+    //if it is a boolean, true or false replace with 0 or 1
+    if (str == "true"){
+        str = 1;
+        return str;
+    } else if (str == "false"){
+        str = 0;
+        return str;
+    }
+
+    //if it is a function call
+    if (str.includes("(") && str.endsWith(")")){
+        //args = between first open parenthesis and last close parenthesis, split by comma
+        let args = str.slice(str.indexOf("(")+1, str.lastIndexOf(")"))
+        args = args.split(",")
+        args = args.map(arg => arg.trim()) //remove whitespace
+        for (let j = 0; j < args.length; j++) {
+            args[j] = strRender(args[j]);
+            if (args[j] === undefined) return undefined;
+        }
+
+        const func = str.slice(0, str.indexOf("("))
+        str = fn(func, args)
+        return str;
+    }
+
+    log(`Unkown ${str}`, 'error')
+    return undefined;
 }
